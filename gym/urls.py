1b236865c1432e89101472482497fa4b1beeada6
Sirat007 <160517598+Sirat007@users.noreply.github.com>
2025-01-24 17:53:29 +0600

    Update urls.py

--- gym/urls.py
+++ gym/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from .views import FitnessClassView,BookingView,FitnessClassDetailView,BookingDetailView,FitnessClassCreate,FitnessClassUpdate,BookingHistory
+from rest_framework.routers import DefaultRouter
 
+router=DefaultRouter()
 
 urlpatterns = [
     path('list',FitnessClassView.as_view(),name="fitnessclass"),
     path('create/',FitnessClassCreate.as_view(),name='create'),
     path('list/<int:pk>',FitnessClassDetailView.as_view(),name='classdetail'),
     path('delete/<int:pk>',FitnessClassUpdate.as_view(),name='deletecls'),
-    path('booking/',BookingView.as_view(),name="booking"),
+    #path('booking/',BookingView.as_view(),name="booking"),
     path('bookinglist/',BookingHistory.as_view(),name='bookinghistory'),
     path('booking/<int:pk>',BookingDetailView.as_view(),name='bookingdetail'),
 ]
+
+router.register(r'booking',BookingView, basename='booking')
+
+urlpatterns = router.urls
