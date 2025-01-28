cd73e87a1fdd159068d8a92c2fc10a99d0c9f64d
Sirat007 <160517598+Sirat007@users.noreply.github.com>
2025-01-28 22:21:05 +0600

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
-from .views import UserRegistrationApiView,UserLoginApiView,UserLogutView,MemberListView,MemberProfleView,MemberDeleteView,activate,PlanCreate,PlanList,PlanDetail
+from django.urls import path,include
+from .views import UserRegistrationApiView,UserLoginApiView,UserLogutView,MemberListView,MemberProfleView,MemberDeleteView,activate,PlanCreate,PlanList,PlanDetail,UserDetail
 from . import views
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 
+router.register(r'profile',MemberProfleView, basename='profile')
+router.register(r'detail',UserDetail,basename='detail')
 urlpatterns = [
     path('register/',UserRegistrationApiView.as_view(), name='register'),
     path('login/',UserLoginApiView.as_view(), name='login'),
@@ -17,8 +19,7 @@ urlpatterns = [
     path('planlist/',PlanList.as_view(),name='plan'),
     path('plancreate/',PlanCreate.as_view(),name='plancreate'),
     path('planedit/<int:pk>',PlanDetail.as_view(),name='planedit'),
-   
+    path('',include(router.urls)),
 ]
 
-router.register(r'profile',MemberProfleView, basename='profile')
-urlpatterns += router.urls
+
