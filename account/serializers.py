190e7268d1893662e83f00ae9b23ee3d6c444f14
Sirat007 <160517598+Sirat007@users.noreply.github.com>
2025-01-28 18:18:37 +0600

    Update serializers.py

--- account/serializers.py
+++ account/serializers.py
@@ -60,7 +60,7 @@ class MemDelSerializer(serializers.ModelSerializer):
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model=CustomUser
-        fields=['first_name', 'last_name', 'email','user_type']
+        fields='__all__'
           
 class MemberPlan(serializers.ModelSerializer):
     class Meta:
