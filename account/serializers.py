efbadd0a7d046dc5592a3dff8ff684796558ff63
Sirat007 <160517598+Sirat007@users.noreply.github.com>
2025-01-31 17:11:45 +0600

    Update serializers.py

--- account/serializers.py
+++ account/serializers.py
@@ -14,12 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
         email = self.validated_data['email']
         password = self.validated_data['password']
         password2 = self.validated_data['confirm_password']
+        user_type = self.validated_data['user_type']
         
         if password != password2:
             raise serializers.ValidationError({'error' : "Password Doesn't Mactched"})
         if CustomUser.objects.filter(email=email).exists():
             raise serializers.ValidationError({'error' : "Email Already exists"})
-        account =CustomUser(username = username, email=email, first_name = first_name, last_name = last_name)
+        account =CustomUser(username = username, email=email, first_name = first_name, last_name = last_name,user_type=user_type)
         print(account)
         account.set_password(password)
         account.is_active = False
@@ -38,7 +39,8 @@ class UserSerializer(serializers.ModelSerializer):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            user_type=validated_data['user_type']
         )
         return user
 
